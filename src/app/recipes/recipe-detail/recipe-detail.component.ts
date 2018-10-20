a4d1486f9fad55f81f25f67ad57f0a47c00c40d1
Gilles Mifsud <gil.mifsud@gmail.com>
2018-10-20 14:38:31 +0200

    10-114 Done

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,17 +1,24 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
+import {Ingredient} from '../../shared/ingredients.model';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.scss']
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.scss']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+    @Input() recipe: Recipe;
 
-  constructor() { }
+    constructor(private recipeService: RecipeService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onAddToShoppingList() {
+        this.recipeService.addIngredientToShoppingList(this.recipe.ingredients);
+    }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,31 @@
-import { EventEmitter } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredients.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('The name', 'Stir fried', 'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg'),
-    new Recipe('The name 2', 'Nachos', 'https://www.seriouseats.com/recipes/images/2017/06/20170627-summer-nachos-matt-clifton-1-1500x1125.jpg'),
-    new Recipe('The name 3', 'Lamb shank', 'http://cdnwp.audiencemedia.com/wp-content/uploads/2015/04/513716-1-eng-GB_lamb-shank-recipes.jpg'),
+    new Recipe(
+      'The name',
+      'Stir fried',
+      'https://www.seriouseats.com/recipes/images/2017/03/Stir_Fried_Lo_Mein_20170315_3-edit-1500x1125.jpg',
+      [
+        new Ingredient('eggs' , 3),
+        new Ingredient('tuna' , 4),
+      ]),
   ];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes() {
-    return this.recipes.slice(); // slice make get justa copy, not the actual reference
+    return this.recipes.slice(); // slice make get just a copy, not the actual reference
   }
 
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
 }
