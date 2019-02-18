decf8f31eec83db356d7d46695950f9ea0b72eee
Gilles MIFSUD <gilles.mifsud@netapsys.fr>
2019-02-18 14:55:32 +0100

    Forms done

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,40 +1,59 @@
-import { Injectable} from '@angular/core';
-import { Recipe } from './recipe.model';
-import { Ingredient } from '../shared/ingredients.model';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+
+import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredients.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  private recipes: Recipe[] = [
-    new Recipe(
-      'The name',
-      'Stir fried',
-      'https://picsum.photos/200/300/?random',
-      [
-        new Ingredient('eggs' , 3),
-        new Ingredient('tuna' , 4),
-      ]),
-      new Recipe(
-        'The name 2',
-        'Stir fried 2',
-        'https://picsum.photos/200/300/?random',
-        [
-          new Ingredient('tomatoes' , 3),
-          new Ingredient('apples' , 4),
-        ]),
-  ];
+    recipesChanged = new Subject<Recipe[]>();
+    private recipes: Recipe[] = [
+        new Recipe(
+            'The name',
+            'Stir fried',
+            'https://picsum.photos/200/300/?random',
+            [
+                new Ingredient('eggs', 3),
+                new Ingredient('tuna', 4),
+            ]),
+        new Recipe(
+            'The name 2',
+            'Stir fried 2',
+            'https://picsum.photos/200/300/?random',
+            [
+                new Ingredient('tomatoes', 3),
+                new Ingredient('apples', 4),
+            ]),
+    ];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+    constructor(private shoppingListService: ShoppingListService) {
+    }
 
-  getRecipes() {
-    return this.recipes.slice(); // slice make get just a copy, not the actual reference
-  }
+    getRecipes() {
+        return this.recipes.slice(); // slice make get just a copy, not the actual reference
+    }
 
-  getRecipeById(index:number) {
-    return this.recipes[index];
-  }
+    getRecipeById(index: number) {
+        return this.recipes[index];
+    }
 
-  addIngredientToShoppingList(ingredients: Ingredient[]) {
-    this.shoppingListService.addIngredients(ingredients);
-  }
+    addIngredientToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
 }
